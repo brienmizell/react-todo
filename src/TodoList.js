104418c77b2de35731ca37a56a540ddd9930a022
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-12-03 14:46:17 -0500

    first commit

---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+import React, { component } from "react";
+
+class TodoList extends component {
+  render() {
+    return (
+      <div>
+        <h1>Todo App</h1>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
