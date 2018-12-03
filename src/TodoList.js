7a91d9a5375febd559d28b92a8b6ae00c03c7707
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-12-03 15:23:06 -0500

    ?

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import List from "./List";
 
 class TodoList extends Component {
   constructor(props) {
@@ -20,6 +21,7 @@ class TodoList extends Component {
           />
           <button type="submit">add</button>
         </form>
+        <div />
       </div>
     );
   }
